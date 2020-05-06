e17018c937bca0da57f21cde275b95d6c609bebe
Simon Lindgren <simon.g.lindgren@gmail.com>
2020-05-06 09:51:53 +0200

    Added timeutil and EncryptionKey and AuditEvent

--- api-server/internal/model/model.go
+++ api-server/internal/model/model.go
@@ -1,7 +1,13 @@
 package model
 
 import (
+	"encoding/base64"
+	"fmt"
 	"time"
+
+	"github.com/CzarSimon/httputil/crypto"
+	"github.com/CzarSimon/httputil/id"
+	"github.com/CzarSimon/webca/api-server/internal/timeutil"
 )
 
 // Certificate types
@@ -14,7 +20,13 @@ const (
 // AccountRequest account creation and authentication request.
 type AccountRequest struct {
 	Name     string `json:"name,omitempty"`
-	Password string `json:"name,omitempty"`
+	Password string `json:"password,omitempty"`
+}
+
+// LoginResponse authentication response.
+type LoginResponse struct {
+	Token   string  `json:"token,omitempty"`
+	Account Account `json:"account,omitempty"`
 }
 
 // Account user account.
@@ -38,4 +50,61 @@ type Certificate struct {
 	AccountID   string `json:"accountId,omitempty"`
 	CreatedAt   string `json:"createdAt,omitempty"`
 	UpdatedAt   string `json:"updatedAt,omitempty"`
+}
+
+// EncryptionKey key used in an symetric cipher.
+type EncryptionKey struct {
+	ID        string
+	Key       string
+	Length    int
+	CreatedAt time.Time
+}
+
+func (k EncryptionKey) String() string {
+	return fmt.Sprintf("EncryptionKey(id=%s, len=%d, createdAt=%v)", k.ID, k.Length, k.CreatedAt)
+}
+
+// Unwrap decodes and decrypts the encryption key.
+func (k EncryptionKey) Unwrap(key []byte) ([]byte, error) {
+	ciphertext, err := base64.StdEncoding.DecodeString(k.Key)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decode key: %w", err)
+	}
+
+	plaintext, err := crypto.NewCipher(key).Decrypt(ciphertext)
+	if err != nil {
+		return nil, fmt.Errorf("failed to decrypt key: %w", err)
+	}
+
+	return plaintext, nil
+}
+
+// NewEncryptionKey generate a random wrapped encryption key.
+func NewEncryptionKey(key []byte, length int) (EncryptionKey, error) {
+	plaintext := make([]byte, length)
+
+	ciphertext, err := crypto.NewCipher(key).Encrypt(plaintext)
+	if err != nil {
+		return EncryptionKey{}, fmt.Errorf("failed to encrypt key: %w", err)
+	}
+
+	return EncryptionKey{
+		ID:        id.New(),
+		Key:       base64.StdEncoding.EncodeToString(ciphertext),
+		Length:    length,
+		CreatedAt: timeutil.Now(),
+	}, nil
+}
+
+// AuditEvent sensitive activity performed in the system.
+type AuditEvent struct {
+	ID        string    `json:"id,omitempty"`
+	UserID    string    `json:"userId,omitempty"`
+	Activity  string    `json:"activity,omitempty"`
+	Resource  string    `json:"resource,omitempty"`
+	CreatedAt time.Time `json:"createdAt,omitempty"`
+}
+
+func (e AuditEvent) String() string {
+	return fmt.Sprintf("AuditEvent(id=%s, userId=%s, activity=%s, resource=%s, createdAt=%v)", e.ID, e.UserID, e.Activity, e.Resource, e.CreatedAt)
 }
---
+++ api-server/internal/timeutil/timeutil.go
@@ -0,0 +1,8 @@
+package timeutil
+
+import "time"
+
+// Now returns the current time in UTC.
+func Now() time.Time {
+	return time.Now().UTC()
+}
