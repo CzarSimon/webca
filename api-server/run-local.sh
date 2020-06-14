7c10a42f51b7dc5454829b2eb3d6716642c85b2b
Simon Lindgren <simon.g.lindgren@gmail.com>
2020-06-14 21:15:08 +0200

    api-server: changed run local script to use sqlite

--- api-server/run-local.sh
+++ api-server/run-local.sh
@@ -4,7 +4,7 @@ go build
 cd ..
 mv cmd/cmd api-server
 
-export MIGRATIONS_PATH='./resources/db/mysql'
+export MIGRATIONS_PATH='./resources/db/sqlite'
 export JWT_ISSUER='webca'
 export JWT_SECRET_FILE='./resources/testing/jwt-secret.key'
 
@@ -12,15 +12,15 @@ export PASSWORD_SALT_LENGTH='32'
 export PASSWORD_MIN_LENGTH='8'
 export PASSWORD_ENCRYPTION_KEY_FILE='./resources/testing/password-encryption.key'
 
-# export DB_TYPE='sqlite'
-# export DB_FILENAME='./test.db'
+export DB_TYPE='sqlite'
+export DB_FILENAME='./test.db'
 
-export DB_TYPE='mysql'
-export DB_HOST='127.0.0.1'
-export DB_PORT='23306'
-export DB_DATABASE='apiserver'
-export DB_USERNAME='apiserver'
-export DB_PASSWORD_FILE='./resources/testing/database-password.key'
+# export DB_TYPE='mysql'
+# export DB_HOST='127.0.0.1'
+# export DB_PORT='23306'
+# export DB_DATABASE='apiserver'
+# export DB_USERNAME='apiserver'
+# export DB_PASSWORD_FILE='./resources/testing/database-password.key'
 
 export JAEGER_SERVICE_NAME='api-server'
 export JAEGER_SAMPLER_TYPE='const'
