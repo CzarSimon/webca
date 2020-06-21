75e2f677908ce9f0f84f8c13e99d1a5209d986d6
Simon Lindgren <simon.g.lindgren@gmail.com>
2020-06-22 01:01:37 +0200

    scripts - is-new-version: corrected if statment

--- scripts/is-new-version.sh
+++ scripts/is-new-version.sh
@@ -2,7 +2,7 @@
 
 check_version() {
     is_new_package=$(git diff HEAD^ "$1/package.json" | grep '+  "version"')
-    if [ "$is_new_package" = "" ]; then
+    if [ "$is_new_package" != "" ]; then
         printf "true"
         exit 0
     fi
