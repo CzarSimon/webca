65981e5a3497daa10f011e4e055575bcd63cd344
Simon Lindgren <simon.g.lindgren@gmail.com>
2020-07-02 01:35:01 +0200

    Added upating of kubernetes manifests

---
+++ scripts/update-images.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+parse_image() {
+    service_name=$1
+    service_version=$(cat "$service_name/package.json" | jq .version --raw-output)
+    printf "docker.pkg.github.com/czarsimon/webca/$service_name:$service_version"
+}
+
+update_image() {
+    service_name=$1
+    image_name=$(parse_image $service_name)
+    yq write --inplace "k8s/$service_name/deployment.yml" spec.template.spec.containers[0].image $image_name
+}
+
+services=("api-server" "web-app")
+
+for service in "${services[@]}"
+do
+    update_image $service
+    image_name=$(parse_image $service_name)
+    echo "Updated image for service $service. image = $image_name"
+done
