b23c82d3a1a7a1a2e3b3d7e68fa09fb17bcf1634
Simon Lindgren <simon.g.lindgren@gmail.com>
2020-07-11 21:40:36 +0200

    Added authentication to dispatch event trigger

--- scripts/dispatch-github-event.sh
+++ scripts/dispatch-github-event.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
-OWNER='czarsimon'
+OWNER='CzarSimon'
 REPO='webca'
 EVENT_TYPE=$1
+TOKEN=$2
 
 curl \
     -X POST \
+    -H "Authorization: token $TOKEN" \
     -H "Accept: application/vnd.github.v3+json" \
     https://api.github.com/repos/$OWNER/$REPO/dispatches \
     -d "{\"event_type\":\"$EVENT_TYPE\"}"
